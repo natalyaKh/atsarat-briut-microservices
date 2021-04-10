d7f7d7e1ca578e29d3bd0855061b3f5f7919602f
natalyaKh <54761439+natalyaKh@users.noreply.github.com>
2021-04-10 11:57:05 +0300

    Step 0_1 create common module (#58)
    Co-authored-by: smilyk <smilyk1982@gmail.com>

---
+++ common/src/test/java/smilyk/atsarat/common/CommonModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package smilyk.atsarat.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ common/utils/src/main/java/smilyk/atsarat/common/utils/UtilsModuleApplication.java
@@ -0,0 +1,13 @@
+package smilyk.atsarat.common.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilsModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsModuleApplication.class, args);
+    }
+
+}
