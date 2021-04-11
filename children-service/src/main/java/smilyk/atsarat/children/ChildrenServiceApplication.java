e6aa62600e06c07fa19689fff06665b491e0deac
natalyaKh <khrebtiivski@gmail.com>
2021-04-11 16:48:38 +0300

    step 4 add project children service and connect his to rabbit

---
+++ children-service/src/main/java/smilyk/atsarat/children/ChildrenServiceApplication.java
@@ -0,0 +1,14 @@
+package smilyk.atsarat.children;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ChildrenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenServiceApplication.class, args);
+	}
+
+}
---
+++ children-service/src/main/java/smilyk/atsarat/children/model/ChildrenEntity.java
@@ -0,0 +1,117 @@
+package smilyk.atsarat.children.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "children")
+public class ChildrenEntity implements Serializable {
+    private static final long serialVersionUID = -2081283438898844381L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String uuidChild;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String secondName;
+
+    //teudat-zeut - needed for some atsarat briut. save in byCrypt
+    @Column(nullable = false)
+    private String tz;
+
+    private String uuidParent;
+
+    private String uuidRespPers;
+
+    private Boolean deleted = false;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUuidChild() {
+        return uuidChild;
+    }
+
+    public void setUuidChild(String uuidChild) {
+        this.uuidChild = uuidChild;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getTz() {
+        return tz;
+    }
+
+    public void setTz(String tz) {
+        this.tz = tz;
+    }
+
+    public String getUuidParent() {
+        return uuidParent;
+    }
+
+    public void setUuidParent(String uuidParent) {
+        this.uuidParent = uuidParent;
+    }
+
+    public String getUuidRespPers() {
+        return uuidRespPers;
+    }
+
+    public void setUuidRespPers(String uuidRespPers) {
+        this.uuidRespPers = uuidRespPers;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public ChildrenEntity() {
+
+    }
+
+    public ChildrenEntity(long id, String uuidChild, String firstName, String secondName, String tz, String uuidParent, String uuidRespPers, Boolean deleted) {
+        this.id = id;
+        this.uuidChild = uuidChild;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.tz = tz;
+        this.uuidParent = uuidParent;
+        this.uuidRespPers = uuidRespPers;
+        this.deleted = deleted;
+    }
+}
