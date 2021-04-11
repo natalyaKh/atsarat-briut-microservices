fcedefd9c247fb78ae7e2b00602ea01dfd816ed5
natalyaKh <khrebtiivski@gmail.com>
2021-04-11 11:30:09 +0300

    add send email to queue to RabbitMQ

---
+++ user-service/src/main/java/smilyk/atsarat/user/config/RabbitConfig.java
@@ -0,0 +1,66 @@
+package smilyk.atsarat.user.config;
+
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@Configuration
+
+
+public class RabbitConfig {
+    private static final String type = "direct";
+
+    @Value(("${spring.rabbitmq.username}"))
+    String rabbitUserName;
+    @Value(("${spring.rabbitmq.password}"))
+    String rabbitPassword;
+
+    @Value(("${email.queue}"))
+    String emailQueue;
+    @Value(("${email.exchange}"))
+    String emailExchange;
+    @Value(("${email.key}"))
+    String emailRoutingkey;
+
+
+
+
+    @Bean
+    public void createEmailQueue() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setPassword(rabbitPassword);
+        factory.setUsername(rabbitUserName);
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(emailQueue, false, false, false, null);
+            channel.exchangeDeclare(emailExchange, type, true);
+            channel.queueBind(emailQueue, emailExchange, emailRoutingkey);
+        } catch (TimeoutException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate rabbitTemplate(org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
+
