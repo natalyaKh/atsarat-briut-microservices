307421d3afbd35a8b0b1bdacabbc38cb1a0f49a2
natalyaKh <khrebtiivski@gmail.com>
2021-04-14 10:00:23 +0300

    STEP_15 add connection to eureka with user/password in children-service add connection to eureka with user/password in email-service add connection to eureka with user/password in tsofim-service add connection to eureka with user/password in scheduler-service

--- email-service/src/main/java/smilyk/atsarat/email/EmailServiceApplication.java
+++ email-service/src/main/java/smilyk/atsarat/email/EmailServiceApplication.java
@@ -2,7 +2,9 @@ package smilyk.atsarat.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class EmailServiceApplication {
 	public static void main(String[] args) {
--- sceduler-service/src/main/java/smilyk/atsarat/sceduler/SchedulerServiceApplication.java
+++ sceduler-service/src/main/java/smilyk/atsarat/sceduler/SchedulerServiceApplication.java
@@ -2,10 +2,11 @@ package smilyk.atsarat.sceduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-
+@EnableEurekaClient
 @SpringBootApplication
 public class SchedulerServiceApplication {
 
