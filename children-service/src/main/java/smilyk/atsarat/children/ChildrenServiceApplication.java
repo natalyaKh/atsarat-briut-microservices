9966ac6c6cd920d46952170f80f75e840c318082
natalyaKh <54761439+natalyaKh@users.noreply.github.com>
2021-04-14 10:03:25 +0300

    Step 15 add all servers to eureka service #90 (#92)
    * STEP_15
    add spring seciruty properties to Eureka
    add connection to eureka with user/password in user-service
    
    * STEP_15
    add connection to eureka with user/password in children-service
    add connection to eureka with user/password in email-service
    add connection to eureka with user/password in tsofim-service
    add connection to eureka with user/password in scheduler-service

--- children-service/src/main/java/smilyk/atsarat/children/ChildrenServiceApplication.java
+++ children-service/src/main/java/smilyk/atsarat/children/ChildrenServiceApplication.java
@@ -2,9 +2,10 @@ package smilyk.atsarat.children;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class ChildrenServiceApplication {
 
 	public static void main(String[] args) {
--- tsofim-servise/src/main/java/smilyk/atsarat/tsofim/TsofimServiceApplication.java
+++ tsofim-servise/src/main/java/smilyk/atsarat/tsofim/TsofimServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 //@EnableCircuitBreaker
 //@EnableHystrix
 //@EnableHystrixDashboard
+@EnableEurekaClient
 
 public class TsofimServiceApplication {
 
