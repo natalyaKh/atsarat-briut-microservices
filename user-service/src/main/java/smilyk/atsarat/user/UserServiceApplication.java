09d7391a684c2ead6f97ffa0a0367b177ad7e835
natalyaKh <54761439+natalyaKh@users.noreply.github.com>
2021-04-14 09:56:38 +0300

    STEP_15 (#91)
    add spring seciruty properties to Eureka
    add connection to eureka with user/password in user-service

--- user-service/src/main/java/smilyk/atsarat/user/UserServiceApplication.java
+++ user-service/src/main/java/smilyk/atsarat/user/UserServiceApplication.java
@@ -1,14 +1,18 @@
 package smilyk.atsarat.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import smilyk.atsarat.user.security.AppProperties;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "smilyk.atsarat")
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
