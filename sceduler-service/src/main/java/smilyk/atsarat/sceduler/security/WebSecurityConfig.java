e3b57033d3000c09a9e5f8b559f12078f10eb43d
natalyaKh <khrebtiivski@gmail.com>
2021-04-23 15:12:13 +0300

    Step 9 add zuul service #36 zull checked token and if its valid - make redirect to service all services open for zuul by his port admin-service work. Get actuator metrics by Basic Auth (admin service son t work with other variant of auth)

--- sceduler-service/src/main/java/smilyk/atsarat/sceduler/security/WebSecurityConfig.java
+++ sceduler-service/src/main/java/smilyk/atsarat/sceduler/security/WebSecurityConfig.java
@@ -1,26 +1,53 @@
-package smilyk.atsarat.children.security;
+package smilyk.atsarat.sceduler.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final Environment environment;
+    @Value("${gateway.port}")
+    String managementPort;
+    /**
+     * permitAll for zuul-gateway-service
+     */
+//    private int managementPort = Integer.parseInt(environment.getProperty("gateway.port"));
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        int managerPortInt = Integer.parseInt(managementPort);
+        web.ignoring().requestMatchers(forPort(managerPortInt));
+    }
+
+    private RequestMatcher forPort(int port) {
+        return (HttpServletRequest request) -> {
+            return port == request.getLocalPort();
+        };
+    }
 
     @Autowired
-    public WebSecurity(Environment environment) {
+    public WebSecurityConfig(Environment environment) {
         this.environment = environment;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /**
+         * needs for autentication with spring boot admin
+         */
+        http.httpBasic();
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests()
--- zuul-service/src/main/java/smilyk/atsarat/zuul/security/WebSecurityConfig.java
+++ zuul-service/src/main/java/smilyk/atsarat/zuul/security/WebSecurityConfig.java
@@ -1,42 +1,39 @@
-package smilyk.atsarat.tsofim.security;
+package smilyk.atsarat.zuul.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-import java.time.Duration;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final Environment environment;
 
     @Autowired
-    public WebSecurity(Environment environment) {
+    public WebSecurityConfig(Environment environment) {
         this.environment = environment;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.headers().frameOptions().disable();
         http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, environment.getProperty("api.email.verification.url.path")).permitAll()
+            .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+            .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
             .antMatchers("/monitor/health").permitAll()
             .antMatchers("/monitor/**").permitAll()
-            .antMatchers("/v2/api-docs/**").permitAll()
-            .antMatchers("/swagger.json").permitAll()
-            .antMatchers("/swagger-ui.html").permitAll()
-            .antMatchers("/swagger-resources/**").permitAll()
-            .antMatchers("/webjars/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new AuthorizationFilter(authenticationManager(), environment));
+            .anyRequest().authenticated()
+            .and()
+            .addFilter(new AuthorizationFilter(authenticationManager(), environment));
+        http.headers().frameOptions().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-
 }
