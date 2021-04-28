aa64630da52a0b78d0fb29caef78932cbcb381d2
natalyaKh <khrebtiivski@gmail.com>
2021-04-28 11:46:21 +0300

    fix bug with rabbitMQ (didnt created queue). adding rabbit for custom container with logic of creating queue with start adding custom rabbit in compose.y
    fix bug with @Order in security

--- zuul-service/src/main/java/smilyk/atsarat/zuul/security/WebSecurity.java
+++ zuul-service/src/main/java/smilyk/atsarat/zuul/security/WebSecurity.java
@@ -1,38 +1,38 @@
-package smilyk.atsarat.zuul.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-    private final Environment environment;
-
-    @Autowired
-    public WebSecurity(Environment environment) {
-        this.environment = environment;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-            .antMatchers(HttpMethod.GET, environment.getProperty("api.email.verification.url.path")).permitAll()
-            .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
-            .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
-            .antMatchers("/monitor/health").permitAll()
-            .antMatchers("/monitor/**").permitAll().anyRequest().authenticated()
-            .and()
-            .addFilter(new AuthorizationFilter(authenticationManager(), environment));
-        http.headers().frameOptions().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-}
+//package smilyk.atsarat.zuul.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurity extends WebSecurityConfigurerAdapter {
+//
+//    private final Environment environment;
+//
+//    @Autowired
+//    public WebSecurity(Environment environment) {
+//        this.environment = environment;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        http.authorizeRequests()
+//            .antMatchers(HttpMethod.GET, environment.getProperty("api.email.verification.url.path")).permitAll()
+//            .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+//            .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+//            .antMatchers("/monitor/health").permitAll()
+//            .antMatchers("/monitor/**").permitAll().anyRequest().authenticated()
+//            .and()
+//            .addFilter(new AuthorizationFilter(authenticationManager(), environment));
+//        http.headers().frameOptions().disable();
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//    }
+//}
